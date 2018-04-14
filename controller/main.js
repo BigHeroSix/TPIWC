db32d4e1d4502d19a9c81485f9bde12787e0e1e9
Christian Herrera <herreraChristian1897@gmail.com>
2018-04-13 23:15:14 -0600

    pull commit

---
+++ controller/main.js
@@ -0,0 +1,2 @@
+import SelfComplementary from "/controller/selfComplementaryController.js";
+document.querySelector("auto-complete").addEventListener("complete",(e)=> console.log(e.detail.name)); 
--- controller/selfComplementaryController.js
+++ controller/selfComplementaryController.js
@@ -1,29 +1,49 @@
-class selfComplementary extends  HTMLElement {
-  constructor() {
-  super();
-  }
-  connectedCallback () {
-    //declaracion de variables de elementos
-    let shadowRoot = this.attachShadow({mode:'open', 'composed':true, 'bubbles':true});
-    let div = document.createElement('div');
-    let search = document.createElement('input');
-    let list = document.createElement('datalist');
-    //configuracion de atributos
-    list.setAttribute('id','list');
-    search.setAttribute('type','text');
-    search.setAttribute('placeholder','Buscar por...');
-    search.setAttribute('list','list');
-    //configuracion de tree
-    shadowRoot.appendChild(div);
-    div.appendChild(search);
-    div.appendChild(list);
+class SelfComplementary extends HTMLElement {
+    constructor() {
+        super();
+        this.root=this.attachShadow({mode: 'open'});
+    }
+    
+    get path(){
+        return this.getAttribute("path");
+    }
+    
+    
+    connectedCallback() {
+        //declaracion de variables de elementos
+        let div = document.createElement('div');
+        let search = document.createElement('input');
+        let list = document.createElement('datalist');
+        //configuracion de atributos
+        list.setAttribute('id', 'list');
+        search.setAttribute('id', 'search');
+        search.setAttribute('type', 'text');
+        search.setAttribute('placeholder', 'Buscar por...');
+        search.setAttribute('list', 'list');
+        search.onkeypress=_=>event;
+        //configuracion de tree
+        div.appendChild(search);
+        div.appendChild(list);
+                this.root.appendChild(div);
 
-  //    fetch("http://localhost:8080/MantenimientoMiddleWare-web-1.0-SNAPSHOT/webresources/marca")
-  //    .then(response => response.json())
-  //    .then(marcas => console.log(marcas));
 
+//            fetch("http://localhost:8080/MantenimientoMiddleWare-web-1.0-SNAPSHOT/webresources/marca")
+//            .then(response => response.json())
+//            .then(marcas => console.log(marcas));
+                
+    }
 
-  }
+    event(){
+        console.log("aquiiii");
+       if(document.querySelector("#search").value!==null){
+           let evn=new CustomEvent('complete',{'composed':true,'bubbles':true,'detail':{ name:'it work'}});
+           this.dispatchEvent(evn);
+    }
+    
+    
+    
 }
-window.customElements.define('self-complementary',selfComplementary);
-//export< default selfComplementary;
+}
+        window.customElements.define('auto-complete',SelfComplementary);
+
+export default SelfComplementary;
