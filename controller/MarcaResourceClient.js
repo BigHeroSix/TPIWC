7e70154eaa831f5402d4f11384852758c3b64eae
DanielM <daniel.97.molin@gmail.com>
2018-04-29 17:25:23 -0600

    tabla con rest

--- controller/MarcaResourceClient.js
+++ controller/MarcaResourceClient.js
@@ -2,7 +2,7 @@ import AbstractResourceClient from './AbstractResourceClient.js';
 class MarcaResourceClient extends AbstractResourceClient {
   constructor() {
     super();
-    this._url += "/marca";
+    this._url += "/equipo";
   }
   findByNameLike(chars){
     return fetch(this._url + "/nombre/" + chars);
