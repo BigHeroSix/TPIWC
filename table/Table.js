664930f5e8515cdf042a284ab5c44ce278c32986
DanielM <daniel.97.molin@gmail.com>
2018-04-30 00:17:43 -0600

    metodo llenar

--- table/Table.js
+++ table/Table.js
@@ -1,11 +1,11 @@
-class Table extends HTMLElement{
-    constructor(lista){
+class Table extends HTMLElement {
+    constructor(lista) {
         super();
-        this._root = this.attachShadow({mode:'open'});        
+        this._root = this.attachShadow({ mode: 'open' });
     }
 
-    connectedCallback(){
-        if(this.tittle){
+    connectedCallback() {
+        if (this.tittle) {
             let tittleDiv = document.createElement("div");
             //let tittleText = document.createAttribute("h4");
             //tittleText.innerText = this.tittle;
@@ -14,59 +14,92 @@ class Table extends HTMLElement{
         }
     }
 
-    setLista(lista){
-        console.log(lista[0].idModelo.idModelo);
-        
+    setLista(lista) {
+        //paginado
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         this.lista = lista;
-        console.log("lista "+this.lista[0].nombre);
-        let columns = this.querySelectorAll("wc-table-column");
+
+        this.columns = this.querySelectorAll("wc-table-column");
         let table = document.createElement("table");
         let thead = document.createElement("thead");
         let tr = document.createElement("tr");
-        columns.forEach( (column)=>{
+        this.columns.forEach((column) => {
             let th = document.createElement("th");
             th.innerText = column.getAttribute("header");
             tr.appendChild(th);
-        } );
+        });
         thead.appendChild(tr);
 
-        let tbody= document.createElement("tbody");
-        this.lista.forEach( (row, index)=>{
-            let tr = document.createElement("tr");
-            columns.forEach( (column)=>{
-                let td = document.createElement("td");
-                let campos = column.getAttribute('value').split(".");
-                var campo = row[`${campos[0]}`];
-                console.log("campo "+campo);
-                if(campos.length > 1){
-                    for(let i = 0 ; i<campos.length-1; i++){
-                        campo = campo[`${campos[i+1]}`];
-                        if(campo.length===1){break;}
-                    }
-                }
-                td.innerText = campo;
-                tr.appendChild(td);
-            });
-            tbody.appendChild(tr);
-        });
+        //METODO
+        var tbody;
+        if(this.paginator){
+            tbody = this.llenarTabla(0,5); 
+        }else{
+            tbody = this.llenarTabla(0,this.lista.length);
+        }
+        
+
         table.appendChild(thead);
         table.appendChild(tbody);
         this._root.appendChild(table);
     }
 
 
+    llenarTabla(first,pagesize){
+        let tbody = document.createElement("tbody");
+        
+        //first y pagesize diferentes
+        for (let i = first; i < pagesize + first; i++) {
+            if (this.lista[i] == undefined) { break; }
+            let tr = document.createElement("tr");
+            this.columns.forEach((column) => {
+                let td = document.createElement("td");
+                let campos = column.getAttribute('value').split(".");
+                var campo = this.lista[i][`${campos[0]}`];
 
-    get getLista(){
+                if (campos.length > 1) {
+                    for (let i = 0; i < campos.length - 1; i++) {
+                        campo = campo[`${campos[i + 1]}`];
+                        if (campo.length === 1) { break; }
+                    }
+                }
+                td.innerText = campo;
+                tr.appendChild(td);
+            });
+            tbody.appendChild(tr);
+        }
+        return tbody;
+    }
+
+
+    get paginator(){
+        return this.getAttribute("paginator");
+    }
+
+    get getLista() {
         return this.lista;
     }
 
-    get tittle(){
+    get tittle() {
         return this.getAttribute("tittle");
     }
-    set ttitle(tittle){
-        this.setAttribute("tittle",tittle);
+    set ttitle(tittle) {
+        this.setAttribute("tittle", tittle);
     }
 }
 
-customElements.define("wc-table",Table);
+customElements.define("wc-table", Table);
 export default Table;
