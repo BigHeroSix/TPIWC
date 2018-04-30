6b4a8214afe77cdbf51c4344f59a7bfb8a8317d1
Christian Herrera <herreraChristian1897@gmail.com>
2018-04-30 12:48:49 -0600

    ordenamiento de tabla

--- table/Table.js
+++ table/Table.js
@@ -11,26 +11,29 @@ class Table extends HTMLElement {
             //tittleText.innerText = this.tittle;
             tittleDiv.innerText = this.tittle;
             this._root.appendChild(tittleDiv);
-             
-            
         }
-    
     }
 
 
-    setLista(lista){
-        
+    setLista(lista) {
+
         this.lista = lista;
         this.columns = this.querySelectorAll("wc-table-column");
         let table = document.createElement("table");
         let thead = document.createElement("thead");
         let tr = document.createElement("tr");
-        this.columns.forEach((column) => {
+        this.columns.forEach((column, index) => {
+
             let th = document.createElement("th");
+            if (column.getAttribute("sortable") !== null) {
+                th.onclick = (e) => this.sortTable(index);
+            }
+
+
             th.innerText = column.getAttribute("header");
             //style
             th.style.border = "black 1px solid";
-            th.style.padding = "3px"
+            th.style.padding = "3px";
 
             tr.appendChild(th);
         });
@@ -38,38 +41,38 @@ class Table extends HTMLElement {
 
         //METODO
         var tbody;
-        if(this.paginator){
-            tbody = this.llenarTabla(0,5); 
-        }else{
-            tbody = this.llenarTabla(0,this.lista.length);
+        if (this.paginator) {
+            tbody = this.llenarTabla(0, 5);
+        } else {
+            tbody = this.llenarTabla(0, this.lista.length);
         }
-        
-        table.style.borderCollapse="collapse";
-        
+
+        table.style.borderCollapse = "collapse";
+
         table.appendChild(thead);
         table.appendChild(tbody);
         this._root.appendChild(table);
     }
 
 
-    llenarTabla(first,pagesize){
+    llenarTabla(first, pagesize) {
         let tbody = document.createElement("tbody");
-        
+
         //first y pagesize diferentes
         for (let i = first; i < pagesize + first; i++) {
             if (this.lista[i] == undefined) { break; }
             let tr = document.createElement("tr");
-            tr.onclick=(e)=>{
-                let rows=this._root.querySelectorAll("tr");
+            tr.onclick = (e) => {
+                let rows = this._root.querySelectorAll("tr");
 
-                rows.forEach((value)=>{
-                value.style.background="inherit";
+                rows.forEach((value) => {
+                    value.style.background = "inherit";
                 });
-                tr.style.background="#C1E5EB"
-                let rowSelected=tr.sectionRowIndex;
-                console.log("ID Marca seleccionada: "+this.lista[rowSelected].idMarca);
+                tr.style.background = "#C1E5EB"
+                let rowSelected = tr.sectionRowIndex;
+                console.log("ID Marca seleccionada: " + this.lista[rowSelected].idMarca);
             }
-            this.columns.forEach( (column)=>{
+            this.columns.forEach((column) => {
                 let td = document.createElement("td");
                 //style
                 td.style.border = "black 1px solid";
@@ -88,13 +91,64 @@ class Table extends HTMLElement {
                 tr.appendChild(td);
             });
             tbody.appendChild(tr);
-            
+
         }
         return tbody;
     }
 
+    sortTable(n) {
+        let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = this._root.querySelector("table");
+        switching = true;
+        dir = "asc";
+        while (switching) {
+            switching = false;
+            rows = table.getElementsByTagName("TR");
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[n];
+                y = rows[i + 1].getElementsByTagName("TD")[n];
+                if (dir == "asc") {
+                    if (parseInt(x.textContent == "NaN")) {
+                        if (x.innerText.toLowerCase() > y.innerText.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else {
+                        if (x.innerText > y.innerText) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                } else if (dir == "desc") {
+                    if (parseInt(x.textContent == "NaN")) {
+                        if (x.innerText.toLowerCase() < y.innerText.toLowerCase()) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else {
+                        if (x.innerText < y.innerText) {
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchcount++;
+            } else {
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
 
-    get paginator(){
+
+    get paginator() {
         return this.getAttribute("paginator");
     }
 
