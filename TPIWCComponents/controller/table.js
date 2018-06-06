1f39b6659ce1ed6b95ff0611b6c26d731b49f96d
katiro <katirox9@gmail.com>
2018-06-05 18:54:07 -0600

    table arreglado

--- TPIWCComponents/controller/table.js
+++ TPIWCComponents/controller/table.js
@@ -16,16 +16,24 @@ customElements.whenDefined('vaadin-grid').then(_ => {
 function tableConstructor(json) {
     console.log(json[0]);
     let Propiedades = Object.keys(json[0]);
+    let Objetos = Object.values(json[0]);
     console.log(Propiedades);
-    
+    console.log(Objetos);
+
     let table = document.querySelector("vaadin-grid");
     for (let index = 0; index < Propiedades.length; index++) {
         let vaadinColum = document.createElement("vaadin-grid-column");
         let templete1 = document.createElement("template");
         templete1.setAttribute("class", "header");
-        templete1.innerHTML = Propiedades[index];
         let templete2 = document.createElement("template");
-        templete2.innerHTML = '[[item.' + Propiedades[index] + ']]';
+        console.log(typeof Objetos[index]);
+        templete1.innerHTML = Propiedades[index];
+        if (typeof Objetos[index] == 'object') {
+            templete2.innerHTML = '[[item.' + Propiedades[index] +'.'+ Propiedades[index] + ']]';
+        } else {
+            templete2.innerHTML = '[[item.' + Propiedades[index] + ']]';
+        }
+
         vaadinColum.appendChild(templete1);
         vaadinColum.appendChild(templete2);
         table.appendChild(vaadinColum);
