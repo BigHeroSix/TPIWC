fd64f6de4523dc5b4b4dfc25746db85e4d1b3aaf
katiro <katirox9@gmail.com>
2018-06-05 18:10:28 -0600

    table General

--- TPIWCComponents/controller/table.js
+++ TPIWCComponents/controller/table.js
@@ -5,9 +5,31 @@ customElements.whenDefined('vaadin-grid').then(_ => {
     let marca = new MarcaResourceClient();
     table.dataProvider = (params, callback) => {
         marca.findByRange(0, 4).then(r => {
-           return r.json();
+            return r.json();
         }).then(data => {
-            callback(data,data.length);
+            tableConstructor(data);
+            callback(data, data.length);
         });
     }
 });
+
+function tableConstructor(json) {
+    console.log(json[0]);
+    let Propiedades = Object.keys(json[0]);
+    console.log(Propiedades);
+    
+    let table = document.querySelector("vaadin-grid");
+    for (let index = 0; index < Propiedades.length; index++) {
+        let vaadinColum = document.createElement("vaadin-grid-column");
+        let templete1 = document.createElement("template");
+        templete1.setAttribute("class", "header");
+        templete1.innerHTML = Propiedades[index];
+        let templete2 = document.createElement("template");
+        templete2.innerHTML = '[[item.' + Propiedades[index] + ']]';
+        vaadinColum.appendChild(templete1);
+        vaadinColum.appendChild(templete2);
+        table.appendChild(vaadinColum);
+    }
+
+
+}
