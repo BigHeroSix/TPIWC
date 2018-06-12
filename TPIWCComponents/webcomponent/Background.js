115b036e5be9548ccecc9dd2f2218fd67d1d43c8
Luis Doratt <luisdoratt@hotmail.com>
2018-06-11 22:34:22 -0600

    Avance pantalla 11

--- TPIWCComponents/webcomponent/Background.js
+++ TPIWCComponents/webcomponent/Background.js
@@ -7,30 +7,30 @@ class BackgroundMarca extends HTMLElement {
     }
 
     connectedCallback() {
-       
-        this.addEventListener("paginatorOnload",(e)=>{
+
+        this.addEventListener("paginatorOnload", (e) => {
             let table = document.querySelector('wc-table');
             table.dataProvider(e.detail.jsonData);
         });
 
-        this.addEventListener("onpagesize",(e)=>{
+        this.addEventListener("onpagesize", (e) => {
             let table = document.querySelector('wc-table');
             table.dataProvider(e.detail.jsonData);
         });
 
-        addEventListener("selectedRow",(e)=>{
-            let modal=document.querySelector("wc-modal-dialog");
-            for(let i=0;i<e.detail.headers.length;i++){
-                let p=document.createElement("p");
-                p.innerHTML=`${e.detail.headers[i].textContent}:     ${e.detail.source[i].textContent}`;
+        addEventListener("selectedRow", (e) => {
+            let modal = document.querySelector("wc-modal-dialog");
+            for (let i = 0; i < e.detail.headers.length; i++) {
+                let p = document.createElement("p");
+                p.innerHTML = `${e.detail.headers[i].textContent}:     ${e.detail.source[i].textContent}`;
                 modal.appendChild(p);
             }
-           
+
             modal.init();
             modal.toggleVisibility(true);
-            
+
         });
-        
+
         this.addEventListener("complete", (e) => {
             let service = new MarcaResourceClient();
             service.findByNameLike(e.detail.char)
@@ -53,6 +53,9 @@ class BackgroundMarca extends HTMLElement {
             }
         }); */
     }
+
+
+
 }
 customElements.define("background-marca", BackgroundMarca);
 export default BackgroundMarca;
