c3fa18625cd565c962711c58566576edd8b6affa
Christian Herrera <herreraChristian1897@gmail.com>
2018-06-14 12:38:58 -0600

    filtado por usuario fecha y correlativo

--- TPIWCComponents/controller/ordenesFinalizadas.js
+++ TPIWCComponents/controller/ordenesFinalizadas.js
@@ -1,37 +1,49 @@
 import OrdenTrabajoResourceClient from "../boundary/OrdenTrabajoResourceClient.js"
 var otr=new OrdenTrabajoResourceClient(); 
 
-customElements.whenDefined("vaadin-text-field")
+customElements.whenDefined("vaadin-text-field","vaadin-button","vaadin-date-picker")
 .then(e=>{  
-   let filter; 
+   let filter;
+   let datepicker=document.querySelector("vaadin-date-picker");
    let textfield=document.querySelector("vaadin-text-field");
+   let btn=document.querySelector("vaadin-button");
    let value;
-textfield.addEventListener("change",e=>{
-    filter=textfield.value;
-    filtrar(filter);
-    let d=new Date(filter);
-    textfield.value=null
-   /* otr.byDate(d) 
-    .then(r=>{
-        return r.json();
-    })
-    .then(a=>{
-        console.log(a);
-    })*/
-});
+
+   if(textfield.focus()){
+       datepicker.value=null;
+   }
+ 
+btn.addEventListener("click",e=>{
+   if(datepicker.value!=null&&datepicker.value!=""){
+       let date=datepicker.value.split("-");
+       let dateFormat=date[0]+"-"+date[2]+"-"+date[1];
+       otr.byDate(dateFormat)
+       .then(response=>{
+           return response.json();
+       })
+       .then(data=>{
+        document.querySelector("wc-table").dataProvider(data);
+
+       })
+   }
+   if(textfield.value!=null){
+       filtrar(textfield.value);
+   }
+   textfield.value=null
+   datepicker.value=null;
+});  
+
+
+ 
 
 });
 
 function filtrar(filter){
-    console.log(typeof filter);
-    otr=new OrdenTrabajoResourceClient(); 
-
     otr.getbyFilter(0,1,filter).
 then(response=>{
     return response.json();
 })
 .then(data=>{
-    console.log(data);
     document.querySelector("wc-table").dataProvider(data);
 });
 }
